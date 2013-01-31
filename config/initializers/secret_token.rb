9f993c59431d072d074500813e55f99fd0494159
Matthew Holloway <matthew.holloway@logicwerx.com>
2013-01-30 21:17:58 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twheeler::Application.config.secret_token = '87b098322af51cc26df27a3d8b9440a0da2bc7eef4aa0788abae175b18ff55ac9bc0231a4054069e44fc8e6969d4f9806a4ecbe3d7e7d55e7a472a11efc035a3'
